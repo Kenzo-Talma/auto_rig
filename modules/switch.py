04b13da7b34d426b76b915d7bccb46e8a92b733a
unknown <kenzotalma@gmail.com>
2024-06-24 12:17:36 +0200

    add ik fk switch function

--- modules/switch.py
+++ modules/switch.py
@@ -1,7 +1,8 @@
 import maya.cmds as cmds
-from tools.attr_lyb import edit_attr, connect_attr
+from tools.attr_lyb import edit_attr, connect_attr, add_attr
 from tools.create_control import create_control
 from tools.list_lyb import append_list, extend_list
+from tools.create_node import create_node
 
 
 class Ribbon_Module:
@@ -48,7 +49,7 @@ class Ribbon_Module:
 
         # add object to list
         extend_list(self.transfrom, [ctrl, grp])
-        append_list(self.shape, curve)
+        append_list(self.shapes, curve)
         append_list(self.control, ctrl)
 
         # lock attribute
@@ -73,3 +74,102 @@ class Ribbon_Module:
             offset[1],
             offset[2]
         )
+
+    def create_blend_matrix(self, input_list, output):
+        # check if input list is instance
+        if not isinstance(input_list, list):
+            input_list = [input_list]
+
+        # create blend matrix
+        blm = create_node('blendMatrix', n=output.rpartition('_')[0]+'_blm')
+
+        # create blend list
+        blend_list = []
+
+        # input loop
+        for n, input in enumerate(input_list):
+            # check if input got parent
+            if cmds.listRelatives(input, p=True):
+                # create mult matrix node
+                input_mlm = create_node(
+                    'multMatrix',
+                    n=input.rpartition('_')[0]+'_mlm'
+                )
+                connect_attr(
+                    input+'.worldMatrix',
+                    input_mlm+'.matrixIn[0]',
+                    f=True
+                )
+                connect_attr(
+                    cmds.listRelatives(input, p=True)[0]+'.worldInverseMatrix',
+                    input_mlm+'.matrixIn[1]',
+                    f=True
+                )
+                connect_attr(
+                    input_mlm+'.matrixSum',
+                    f'{blm}.target[{str(n)}].targetMatrix',
+                    f=True
+                )
+            else:
+                connect_attr(
+                    input+'.worldMatrix',
+                    f'{blm}.target[{str(n)}].targetMatrix',
+                    f=True
+                )
+
+            # connect output
+            connect_attr(
+                blm+'.outputMatrix',
+                output+'.offsetParentMatrix',
+                f=True
+            )
+
+            # add blend to blend list
+            blend_list.append(f'{blm}.target[{str(n)}].weight')
+
+        return blm, blend_list
+
+    def ik_fk_switch(
+        self,
+        main_joint_list,
+        ik_joint_list,
+        fk_joint_list,
+        ik_ctrl_list,
+        fk_ctrl_list
+    ):
+        # create attribute
+        ik_fk_attr = add_attr(
+            self.control[0],
+            long_name='switch_ik_fk',
+            attribute_type='enum',
+            enumNames='ik:fk'
+        )
+        # create inverse node
+        ik_fk_rev = create_node('reverse', n=self.full_name+'_ik_fk_rev')
+        connect_attr(ik_fk_attr, ik_fk_rev+'.input1X', f=True)
+        ik_fk_rev_attr = ik_fk_rev+'.input1X'
+
+        # connect ik fk and main
+        for main, ik, fk in zip(main_joint_list, ik_joint_list, fk_joint_list):
+            blm, blend_list = self.create_blend_matrix([ik, fk], main)
+
+            # connect attribute to blend matrix
+            connect_attr(ik_fk_rev_attr, blend_list[0], f=True)
+            connect_attr(ik_fk_attr, blend_list[1])
+
+        # connect visibility
+        for ctrl in ik_ctrl_list:
+            for shape in cmds.listRelatives(ctrl, s=True):
+                connect_attr(
+                    ik_fk_rev_attr,
+                    shape+'.v',
+                    f=True
+                )
+
+        for ctrl in fk_ctrl_list:
+            for shape in cmds.listRelatives(ctrl, s=True):
+                connect_attr(
+                    ik_fk_rev_attr,
+                    shape+'.v',
+                    f=True
+                )
