a9058bcf25fc6671cd20fb70a9dd77275f83dda3
unknown <kenzotalma@gmail.com>
2024-07-04 20:09:10 +0200

    --no_message

--- UI/type_dictionnary_modify.py
+++ UI/type_dictionnary_modify.py
@@ -19,34 +19,32 @@ class MainWin(QtWidgets.QDialog):
 
     def __init__(self, parent=mayaMainWindow()):
         super(MainWin, self).__init__(parent)
-        self.edgeloop1 = None
-        self.edgeloop2 = None
-        self.setWindowTitle("Ribbon maker")
-        # self.setMinimumWidth(200)
-        # self.setMaximumWidth(500)
+
+        # windowns setting
+        self.setWindowTitle("type extention list")
+        self.setMinimumWidth(290)
+        self.setMinimumHeight(320)
         self.setWindowFlag(QtCore.Qt.WindowContextHelpButtonHint, False)
 
-        self.setGeometry(200, 500, 400, 300)
-
-        self.tool_box = QtWidgets.QToolBox(self)
-        self.mainLayout = QtWidgets.QVBoxLayout(self)
-
+        # dictionary
         self.type_dic = {}
+        self.line_dic = {}
 
+        # init functions
         self.fill_dic()
-        self.createWidget()
+        self.create_ui()
 
-    def click_method(self, identifier):
-        dic = self.widgets[identifier]
+    def line_edit_func(self, identifier):
+        dic = self.line_dic[identifier]
         line = dic['lineEdit']
 
         self.type_dic[identifier] = line.text()
-        print(line.text)
+        print(line.text())
 
-    def close_func(self):
+    def write_func(self):
         path = 'D:/K.Work/git/auto_rig/ressources/type_dictionary.json'
 
-        control_shape_file = open(Path(path))
+        control_shape_file = open(Path(path), 'w')
 
         json.dump(self.type_dic, control_shape_file, indent=4)
 
@@ -58,36 +56,58 @@ class MainWin(QtWidgets.QDialog):
         self.type_dic = json.load(control_shape_file)
 
     # widgets
-    def createWidget(self):
-        self.mainLayout.addWidget(self.tool_box)
+    def create_ui(self):
+        # create layouts ad widgets
+        self.type_layout = QtWidgets.QVBoxLayout(self)
 
-        self.widgets = {}
+        self.main_widget = QtWidgets.QWidget(self)
+
+        self.scroll_area = QtWidgets.QScrollArea(self)
+
+        # type dictionary loop
         for ob_type in self.type_dic:
+            # create line dictionary
             identifier = ob_type
             label = QtWidgets.QLabel(ob_type)
-            line = QtWidgets.QLineEdit(ob_type)
-            self.widgets[identifier] = {
+            line = QtWidgets.QLineEdit(self.type_dic[ob_type])
+
+            # add entry
+            self.line_dic[identifier] = {
                 'label': label,
                 'lineEdit': line,
-                'func': lambda check=None, a=identifier: self.click_method(a)
+                'func': lambda check=None, a=identifier: self.line_edit_func(a)
             }
 
-        for widget in self.widgets:
-            button_dic = self.widgets[widget]
+        # create widgets
+        for widget in self.line_dic:
+            # open line dictionary
+            button_dic = self.line_dic[widget]
             label = button_dic['label']
             line = button_dic['lineEdit']
-            line.textEdited.connect(button_dic['func'])
 
-            # self.mainLayout.addWidget(label)
-            # self.mainLayout.addWidget(line)
+            # define connect
+            line.returnPressed.connect(button_dic['func'])
 
-            self.tool_box.addItem(label, widget)
-            self.tool_box.insertItem(line, widget)
+            # create layout
+            layout = QtWidgets.QHBoxLayout(self)
 
-        self.close_button = QtWidgets.QPushButton('close')
-        self.close_button.clicked.connect(self.close_func)
-        # self.mainLayout.addWidget(self.close_button)
-        self.tool_box.addItem(self.close_button, 'close')
+            layout.addWidget(label)
+            layout.addWidget(line)
+
+            # add layout to main type layout
+            self.type_layout.addLayout(layout)
+
+        # set and add close button to close button layout
+        self.close_button = QtWidgets.QPushButton('write extentions')
+        self.close_button.clicked.connect(self.write_func)
+        self.type_layout.addWidget(self.close_button)
+
+        # add type layout to the type widget
+        self.main_widget.setLayout(self.type_layout)
+
+        # add scroll area
+        self.scroll_area.setWidgetResizable(True)
+        self.scroll_area.setWidget(self.main_widget)
 
 
 ##############################################################################
