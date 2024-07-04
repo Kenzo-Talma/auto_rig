d60f4510a32312b460f7306297b71bbfad0e92e2
unknown <kenzotalma@gmail.com>
2024-07-04 13:45:08 +0200

    modify modules

--- modules/limb.py
+++ modules/limb.py
@@ -5,40 +5,40 @@ from tools.create_control import create_control
 from tools.transform_lib import match_transform
 from tools.list_lib import append_list, extend_list
 from tools.joint_lib import simple_joint_chain
+from tools.space_switch import space_switch
 
 
 class Limb_Module:
     # init method
     def __init__(
-            self,
-            switch,
-            space_input_list,
-            data_input_list,
-            name,
-            side,
-            chain_lenght=3,
-            compound_name=None,
+            self
     ):
         # inputs
-        self.switch = switch
-        self.space_input_list = space_input_list
-        self.data_input_list = data_input_list
+        self.switch = None
+        self.space_input_dic = {
+            'limb_start_input': None,
+            'limb_end_input': None
+        }
+        self.data_input_dic = {}
 
         # outputs
         self.space_output = None
         self.data_output = None
 
-        # chain info
-        self.chain_length = chain_lenght
+        # limb info
+        self.limb_length = 3
+        self.start_skip_attr = ['rotate', 'scale', 'shear']
+        self.end_skip_attr = None
 
         # def name
-        self.name = name
-        self.side = side
+        self.name = 'limb_1'
+        self.side = 'C'
+        self.compound_name = None
 
-        if compound_name:
-            self.full_name = f'{side}_{compound_name}_{name}'
+        if self.compound_name:
+            self.full_name = f'{self.side}_{self.compound_name}_{self.name}'
         else:
-            self.full_name = f'{side}_{name}'
+            self.full_name = f'{self.side}_{self.name}'
 
         # module objects
         self.transfrom = None
@@ -51,14 +51,45 @@ class Limb_Module:
         self.guide_list = None
         self.group_list = None
 
-        self.ik_control = None
         self.fk_control = None
-
-        self.ik_joint = None
+        self.fk_group = None
         self.fk_joint = None
 
+        self.ik_control = None
+        self.fk_group = None
+        self.ik_joint = None
+
+        self.main_joint = None
+
+    def add_start_input(self, space_input_added):
+        # get start space input list
+        start_input_list = self.space_input_dic['limb_start_input']
+
+        # add object to list
+        start_input_list = append_list(start_input_list, space_input_added)
+
+        # return list
+        self.space_input_dic['limb_start_input'] = start_input_list
+
+    def add_end_input(self, space_input_added):
+        # get end space input list
+        end_input_list = self.space_input_dic['limb_end_input']
+
+        # add object to list
+        end_input_list = append_list(end_input_list, space_input_added)
+
+        # return list
+        self.space_input_dic['limb_end_input'] = end_input_list
+
+    def add_space_ouput(self):
+        for i in range(self.limb_length):
+            self.space_output = append_list(
+                self.space_output,
+                f'{self.full_name}_{str(i)}_main_jnt'
+            )
+
     def create_guides(self):
-        for i in range(self.chain_length):
+        for i in range(self.limb_length):
             # create guide
             loc = create_node(
                 'spaceLocator',
@@ -77,7 +108,7 @@ class Limb_Module:
         # create joint list
         ik_joint_list = simple_joint_chain(
             self.guide_list,
-            extention=['loc', 'ik_chain']
+            extention=['loc', 'ik_jnt']
         )
 
         # add joints to list
@@ -112,6 +143,9 @@ class Limb_Module:
         cmds.move(0, 0, 5, a=True)
         cmds.poleVectorConstraint(pv_ctrl, ik_handle)
 
+        # connect last joint rotation
+        or_cns = cmds.orientConstraint(ik_ctrl, self.ik_joint[-1])
+
         # add obect to list
         self.transfrom = extend_list(self.transfrom, [
                 ik_ctrl,
@@ -119,6 +153,7 @@ class Limb_Module:
                 pv_ctrl,
                 pv_grp
             ])
+        self.ik_group = extend_list(self.ik_group, [ik_grp, pv_grp])
         self.ik_control = extend_list(self.ik_control, [ik_ctrl, pv_ctrl])
         self.shapes = extend_list(self.shapes, [ik_curve, pv_curve])
         self.transfrom = extend_list(self.transfrom, [
@@ -127,7 +162,11 @@ class Limb_Module:
             ik_ctrl,
             pv_ctrl
         ])
-        self.other_nodes = extend_list(self.other_nodes, [ik_handle, effector])
+        self.other_nodes = extend_list(self.other_nodes, [
+            ik_handle,
+            effector,
+            or_cns
+        ])
 
     def create_fk_limb(self):
         # create joint chain
@@ -152,6 +191,7 @@ class Limb_Module:
 
             # add object to list
             self.transfrom = extend_list(self.transfrom, [grp, ctrl])
+            self.fk_group = append_list(self.fk_group, grp)
             self.fk_control = append_list(self.fk_control, ctrl)
             self.shapes = append_list(self.shapes, curve)
             self.group_list = append_list(self.group_list, grp)
@@ -166,3 +206,57 @@ class Limb_Module:
             # parent group
             if not n == 0:
                 cmds.parent(grp, self.fk_control[n-1])
+
+    def create_ik_fk_switch(self):
+        # create joint chain
+        main_joint_list = simple_joint_chain(
+            self.guide_list,
+            extention=['loc', 'main_jnt']
+        )
+
+        # add joint to list
+        self.joint = extend_list(self.joint, main_joint_list)
+        self.main_joint = extend_list(self.main_joint, main_joint_list)
+
+        # add switch
+        self.switch.ik_fk_switch(
+            self.main_joint,
+            self.ik_joint,
+            self.fk_joint,
+            self.ik_control,
+            self.fk_control
+        )
+
+    def connect_space_inputs(self):
+        # connect start
+        # get data list
+        start_list = self.space_input_dic['limb_start_input']
+        main_parent = start_list[0]
+        start_list.remove(main_parent)
+
+        # connect constraint
+        self.switch.create_space_switch(
+            follow_parent=main_parent,
+            parent_list=start_list,
+            target_list=[self.fk_group[0], self.ik_joint[0]],
+            attribute_name=self.full_name+'_space_switch',
+            attribute_list=None,
+            skip_list=self.start_skip_attr
+        )
+
+        # connect start
+        # get data list
+        end_list = self.space_input_dic['limb_end_input']
+        main_parent = end_list[0]
+        end_list.remove(main_parent)
+
+        # connect constraint
+        space_switch(
+            follow_parent=main_parent,
+            parent_list=end_list,
+            target_list=[self.ik_group[0]],
+            attribute_name=self.ik_control+'_space_switch',
+            attribute_list=None,
+            skip_list=self.end_skip_attr,
+            control=self.ik_control[0]
+        )
